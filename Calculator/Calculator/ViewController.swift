f628799ae587fe8fe559d84aebf7e98aa9c04020
katsuyanakai <katsuyanakai88@gmail.com>
2015-05-02 16:39:44 +0900

    色等変更。

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -15,25 +15,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
 
         
-        let button1 = makeButtonToAddView("AC", x:47.5, y:193);
-        let button2 = makeButtonToAddView("+/-", x:142.5, y:193);
-        let button3 = makeButtonToAddView("%", x:237.5, y:193);
-        let button4 = makeButtonToAddView("➗", x:332.5, y:193);
-        let button5 = makeButtonToAddView("7", x:47.5, y:288);
-        let button6 = makeButtonToAddView("8", x:142.5, y:288);
-        let button7 = makeButtonToAddView("9", x:237.5, y:288);
-        let button8 = makeButtonToAddView("✖️", x:332.5, y:288);
-        let button9 = makeButtonToAddView("4", x:47.5, y:383);
-        let button10 = makeButtonToAddView("5", x:142.5, y:383);
-        let button11 = makeButtonToAddView("6", x:237.5, y:383);
-        let button12 = makeButtonToAddView("-", x:332.5, y:383);
-        let button13 = makeButtonToAddView("1", x:47.5, y:478);
-        let button14 = makeButtonToAddView("2", x:142.5, y:478);
-        let button15 = makeButtonToAddView("3", x:237.5, y:478);
-        let button16 = makeButtonToAddView("+", x:332.5, y:478);
-        let button17 = makeButtonToAddView("0", x:47.5, y:573);
-        let button18 = makeButtonToAddView(".", x:237.5, y:573);
-        let button19 = makeButtonToAddView("=", x:332.5, y:573);
+        let button1 = makeButtonToAddView("AC", x:47.5, y:198);
+        let button2 = makeButtonToAddView("+/-", x:142.5, y:198);
+        let button3 = makeButtonToAddView("%", x:237.5, y:198);
+        let button4 = makeButtonToAddView("÷", x:332.5, y:198);
+        let button5 = makeButtonToAddView("7", x:47.5, y:293);
+        let button6 = makeButtonToAddView("8", x:142.5, y:293);
+        let button7 = makeButtonToAddView("9", x:237.5, y:293);
+        let button8 = makeButtonToAddView("×", x:332.5, y:293);
+        let button9 = makeButtonToAddView("4", x:47.5, y:388);
+        let button10 = makeButtonToAddView("5", x:142.5, y:388);
+        let button11 = makeButtonToAddView("6", x:237.5, y:388);
+        let button12 = makeButtonToAddView("-", x:332.5, y:388);
+        let button13 = makeButtonToAddView("1", x:47.5, y:483);
+        let button14 = makeButtonToAddView("2", x:142.5, y:483);
+        let button15 = makeButtonToAddView("3", x:237.5, y:483);
+        let button16 = makeButtonToAddView("+", x:332.5, y:483);
+        let button17 = makeButtonToAddView("0", x:47.5, y:578);
+        let button18 = makeButtonToAddView(".", x:237.5, y:578);
+        let button19 = makeButtonToAddView("=", x:332.5, y:578);
 
         
         self.view.addSubview(button1)
@@ -56,12 +56,38 @@ class ViewController: UIViewController {
         self.view.addSubview(button18)
         self.view.addSubview(button19)
         
+        self.view.backgroundColor = UIColor.blackColor()
 
+        
+        //背景色 変更
+        button1.backgroundColor = UIColor(red: 205/255, green: 205/255, blue: 205/255, alpha: 1.0)
+        button2.backgroundColor = UIColor(red: 205/255, green: 205/255, blue: 205/255, alpha: 1.0)
+        button3.backgroundColor = UIColor(red: 205/255, green: 205/255, blue: 205/255, alpha: 1.0)
+        button4.backgroundColor = UIColor(red: 235/255, green: 127/255, blue: 42/255, alpha: 1.0)
+        button8.backgroundColor = UIColor(red: 235/255, green: 127/255, blue: 42/255, alpha: 1.0)
+        button12.backgroundColor = UIColor(red: 235/255, green: 127/255, blue: 42/255, alpha: 1.0)
+        button16.backgroundColor = UIColor(red: 235/255, green: 127/255, blue: 42/255, alpha: 1.0)
+        button19.backgroundColor = UIColor(red: 235/255, green: 127/255, blue: 42/255, alpha: 1.0)
+        
+        //テキストの色 変更
+        button4.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        button8.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        button12.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        button16.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        button19.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+
+        //サイズ変更
+        button17.frame = CGRectMake(0, 0, 188, 94)
+        
+        //配置場所
+        button17.layer.position = CGPoint(x:95, y:578)
+
+        
         // Labelを作成.
-        let myLabel: UILabel = UILabel(frame: CGRectMake(0,0,375,191))
+        let myLabel: UILabel = UILabel(frame: CGRectMake(0,0,375,197))
         
         // 背景をオレンジ色にする.
-        myLabel.backgroundColor = UIColor.blueColor()
+        myLabel.backgroundColor = UIColor(red: 124/255, green: 120/255, blue: 120/255, alpha: 1.0)
         
         // 枠を丸くする.
         myLabel.layer.masksToBounds = false
@@ -104,7 +130,7 @@ class ViewController: UIViewController {
         button.setTitle(title, forState: .Normal)
         
         //テキストの色
-        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        button.setTitleColor(UIColor.blackColor(), forState: .Normal)
         
         //タップした状態のテキスト
         button.setTitle(title, forState: .Highlighted)
@@ -119,7 +145,7 @@ class ViewController: UIViewController {
         button.layer.position = CGPoint(x:x, y:y)
         
         //背景色
-        button.backgroundColor = UIColor(red: 0.7, green: 0.2, blue: 0.2, alpha: 0.2)
+        button.backgroundColor = UIColor(red: 217/255, green: 217/255, blue: 217/255, alpha: 1.0)
         
         //角丸
         button.layer.cornerRadius = 0
